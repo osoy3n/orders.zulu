a5f6f7efde65dd1e0a0daeca4f1b6a9edabcf2b1
Yohan D Osorio L <yooslzn1487@gmail.com>
2025-03-08 21:34:13 -0500

    Add basic countries api controller

---
+++ Orders/Orders.Backend/Controllers/CountriesController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Orders.Backend.Data;
+using Orders.Shared.Entities;
+
+namespace Orders.Backend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CountriesController(DataContext context) : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetAsync()
+        {
+            var countries = await context.Countries.ToListAsync();
+            return Ok(countries);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetAsyncId(int id)
+        {
+            var country = await context.Countries.FindAsync(id);
+            if (country == null)
+            {
+                return NotFound();
+            }
+            return Ok(country);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostAsync(Country country)
+        {
+            context.Add(country);
+            await context.SaveChangesAsync();
+            return Ok(country);
+        }
+
+        [HttpPatch]
+        public async Task<IActionResult> PatchAsync(Country country)
+        {
+            context.Update(country);
+            await context.SaveChangesAsync();
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAsync(int id)
+        {
+            var country = await context.Countries.FindAsync(id);
+            if (country == null)
+            {
+                return NotFound();
+            }
+            context.Remove(country);
+            await context.SaveChangesAsync();
+            return NoContent();
+        }
+    }
+}
